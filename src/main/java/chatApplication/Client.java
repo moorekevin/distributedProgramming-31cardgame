6dea31d5e132b0d7a9e98f8ccd531bd9171bb19c
Kevin Moore <hello@moorekevin.dk>
2022-01-06 10:28:28 +0100

    Testing Maven dependencies with ChatApp

---
+++ src/main/java/chatApplication/Client.java
@@ -0,0 +1,61 @@
+package chatApplication;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jspace.*;
+
+public class Client {
+	static String id;
+
+	public static void main(String[] args) {
+		final String uri = "tcp://25.62.120.1:9002/chat?keep";
+		Scanner reader = new Scanner(System.in);
+		try {
+			System.out.println("Enter your username: ");
+			String username = reader.nextLine();
+			
+			RemoteSpace chat = new RemoteSpace(uri);
+			
+			chat.put("user connected", username);
+			
+			id = (String) (chat.get(new ActualField("uniqueid"),new ActualField(username), new FormalField(String.class)))[2];
+						
+			System.out.println("Connected to chat - start chatting!");
+			
+			new Thread(new printMessages(chat)).start();
+			
+			while (true) {
+				String message = reader.nextLine();
+				chat.put("message",username, message);
+			}
+			
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	static class printMessages implements Runnable {
+		Space chat;
+		
+		public printMessages(Space chat) {
+			this.chat = chat;
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				Object[] t;
+				try {
+					t = chat.get(new ActualField(id), new FormalField(String.class), new FormalField(String.class));
+					System.out.println(t[1] + ": " + t[2]);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+}
---
+++ src/main/java/chatApplication/Server.java
@@ -0,0 +1,65 @@
+package chatApplication;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.jspace.*;
+
+public class Server {
+	
+	public static ArrayList<String> users;
+
+	public static void main(String[] args) {
+		users = new ArrayList<String>();
+		SpaceRepository rep = new SpaceRepository();
+		SequentialSpace chat = new SequentialSpace();
+		rep.add("chat", chat);
+
+		final String gateUri = "tcp://25.62.120.1:9002/?keep";
+		rep.addGate(gateUri);
+
+		new Thread(new printChat(chat)).start();
+		
+		System.out.println("Chat server running");
+		
+		while(true) {
+			try {
+				String user = ((String) (chat.get(new ActualField("user connected"), new FormalField(String.class)))[1]);
+				String uniqueID = UUID.randomUUID().toString();
+				users.add(uniqueID);
+				chat.put("uniqueid", user, uniqueID);
+				System.out.println(user + " has connected");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+				
+	}
+	
+	static class printChat implements Runnable {
+		Space chat;
+		
+		public printChat(Space chat) {
+			this.chat = chat;
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					Object[] t = chat.get(new ActualField("message"),new FormalField(String.class), new FormalField(String.class));
+					for (String user : Server.users) {
+						chat.put(user, t[1], t[2]);
+					}
+					System.out.println(t[1] + ": " + t[2]);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
+
+
+}
